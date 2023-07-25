e843b9779a170b8e7491afe2ba35c794d53b5d7d
isnaenti <isnaenti16@gmail.com>
2023-07-25 20:47:42 +0700

    Create debugging_backend_node.js

---
+++ chptr/chapter4/debugging_backend_node.js
@@ -0,0 +1,5 @@
+// Contoh breakpoint di Node.js
+function fungsiContoh() {
+  // Breakpoint akan berhenti di baris berikut saat diinspeksi di IDE atau debugger Node.js
+  console.log('Debugging di backend');
+}
